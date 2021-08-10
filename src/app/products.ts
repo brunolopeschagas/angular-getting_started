ad8dc2721d8e833fc39a10a95eaa6b13b7d33099
brunolopeschagas <brunolopeschagas@gmail.com>
2021-08-10 11:42:01 -0300

    Listando produtos com descrição e observação

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  obs: string;
 }
 
 export const products = [
@@ -10,19 +11,22 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    obs: 'nothing about'
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    obs: ''
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    obs: 'very cheap'
   }
 ];
 
